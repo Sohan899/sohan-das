29d211d043309c04df523b9ce1b93676e19045e3
Sohan899 <143432165+Sohan899@users.noreply.github.com>
2023-10-16 16:07:26 +0530

    Add files via upload

---
+++ salary.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+ {
+ int a,b,c;
+ printf("sides of the triangle=");
+ scanf("%d""%d""%d",&a,&b,&c);
+ if(a=b=c)
+ {
+ 	printf("the triangle is equilateral");
+ }
+ else if(a=b!=c)
+ {
+ 	printf("the triangle is isosceles");
+ }
+ else 
+ { printf("the triangle is scalene");
+ }
+ 
+ 	
+ 	
+ 	
+ 	
+return 0;	
+ }
---
+++ swap without variable.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h.>
+int main()
+{
+int a,b;
+	scanf("%d""%d",&a,&b);
+	a=a+b;
+	b=b-a;
+	b=a-b;
+		printf("the value of a and b respectively is %d %d",a,b);
+	return 0;
+}
